5f08c74a9d855a4ec36c56e6da4081f077cf3702
username1 <email1@email.com>
2022-01-17 16:04:07 +0800

    1

--- src/main/main.ts
+++ src/main/main.ts
@@ -11,17 +11,20 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import path from 'path';
-import { app, BrowserWindow, shell, ipcMain } from 'electron';
-import { autoUpdater } from 'electron-updater';
+import { app, BrowserWindow, shell, ipcMain, autoUpdater } from 'electron';
+// import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
+console.log = log.log
+log.transports.file.level = 'debug';
 
 export default class AppUpdater {
   constructor() {
-    log.transports.file.level = 'info';
-    autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
+
+
+    // autoUpdater.logger = log;
+    // autoUpdater.checkForUpdatesAndNotify();
   }
 }
 
@@ -82,7 +85,7 @@ const createWindow = async () => {
   });
 
   mainWindow.loadURL(resolveHtmlPath('index.html'));
-
+mainWindow.webContents.openDevTools({mode: 'right'})
   mainWindow.on('ready-to-show', () => {
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
@@ -127,6 +130,44 @@ app.on('window-all-closed', () => {
 app
   .whenReady()
   .then(() => {
+    // const feedURL = `file://${path.join(__dirname, 'release/build/squirrel-windows/RELEASES')}`
+    const feedURL = 'http://113.200.234.211:5050/update/windows_64/1.0.3'
+    // const feedURL = 'http://113.200.234.211:5050/download/flavor/default/1.0.3/windows_64/electron-react-boilerplate-1.0.3-full.nupkg'
+
+    console.log(feedURL, 'feedURL')
+    autoUpdater.setFeedURL({url: feedURL})
+    autoUpdater.on('checking-for-update', () => {
+      // 开始检查是否有新版本
+      // 可以在这里提醒用户正在查找新版本
+      console.log('checking-for-update')
+  })
+  
+  autoUpdater.on('update-available', (info: any) => {
+      // 检查到有新版本
+      // 提醒用户已经找到了新版本
+      console.log('update-available', JSON.stringify(info))
+  })
+  
+  autoUpdater.on('update-not-available', (info: any) => {
+      // 检查到无新版本
+      // 提醒用户当前版本已经是最新版，无需更新
+      console.log('update-not-available', JSON.stringify(info))
+  })
+  
+  autoUpdater.on('error', (err) => {
+      // 自动升级遇到错误
+      // 执行原有升级逻辑
+      console.log('error', JSON.stringify(err))
+  })
+  
+  autoUpdater.on('update-downloaded', (ev, releaseNotes, releaseName) => {
+      // 自动升级下载完成
+      // 可以询问用户是否重启应用更新，用户如果同意就可以执行 autoUpdater.quitAndInstall()
+      console.log('update-downloaded', JSON.stringify(releaseNotes))
+  })
+
+  autoUpdater.checkForUpdates();
+
     createWindow();
     app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
